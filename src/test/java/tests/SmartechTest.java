9b759f94c9b105e110600ec0cbff9a140e8ec4a8
dianamishanich <diana-mishanich@ya.ru>
2021-05-28 23:53:19 +0300

    SmartechTest

--- src/test/java/tests/SmartechTest.java
+++ src/test/java/tests/SmartechTest.java
@@ -1,21 +1,35 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class SmartechTest extends BaseTest {
+
+
+public class SmartechTest extends TestBase {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+
+    }
+
+    @BeforeEach
+    public void init() {
+        open("https://smartru.com/");
+    }
 
     @Test
-    @DisplayName("Открытие главной страницы")
+    @DisplayName("Проверка главной страницы")
     void mainPageTest() {
-        open("https://smartru.com/");
         $("body").shouldHave(text("Сервисы и услуги"));
 
     }
@@ -23,9 +37,7 @@ public class SmartechTest extends BaseTest {
     @Test
     @DisplayName("Сервисы и услуги")
     void servicesMenuTest() {
-        open("https://smartru.com/");
         $(".card.border-0[href='/services/software-development/']").click();
-        //$$(byText("Разработка программного обеспечения")).find(visible).parent().click();
         $("body").shouldHave(text("Высоконагруженные приложения"));
 
     }
@@ -33,7 +45,6 @@ public class SmartechTest extends BaseTest {
     @Test
     @DisplayName("Работа главного меню")
     void mainMenuTest() {
-        open("https://smartru.com/");
         $(byText("О нас")).hover();
         $(".dropdown-menu.show").shouldHave(text("История компании"));
         $(".dropdown-menu.show").$(byText("Отзывы")).click();
@@ -48,13 +59,12 @@ public class SmartechTest extends BaseTest {
                 phoneNumber = "+77 777 777 77 77",
                 userEmail = "111111@gmail.com",
                 userMassage = "Привет!",
-                picture = "_53876-66152.jpg";
+                picture = "000.png";
 
-        open("https://smartru.com/");
         $(byText("Обсудить проект")).click();
 
         $("input[placeholder='Ваше имя*']").val(userName);
-        $("input[placeholder='Телефон*']").val(phoneNumber);
+        $$("input[placeholder='Телефон*']").find(visible).val(phoneNumber);
         $("input[placeholder='Email']").val(userEmail);
         $("textarea[placeholder='Сообщение']").val(userMassage);
         $(".inputfile").uploadFile(new File("src/test/resources/" + picture));
@@ -68,10 +78,19 @@ public class SmartechTest extends BaseTest {
     void searchTest() {
         String searchQuery = "о нас";
 
-        open("https://smartru.com/");
         $("#btn-search").click();
         $("input[placeholder='Что вы ищете?']").val(searchQuery).pressEnter();
         $(".main-content").shouldHave(text("использовались Oracle и PostgreSQL"));
+
+    }
+
+    @Test
+    @DisplayName("Проверка карусели со спонсорами")
+    public void carouselUsingTest() {
+        $$("[data-slick-index]").get(8).shouldNotBe(disappear);
+        $(".slick-next.slick-arrow").scrollTo().click();
+        sleep(2000);
+        $$("[data-slick-index]").get(8).shouldBe(visible);
 
     }
 
